04a0a5d6dfd9bcef11ab913e9f1ba6d6d25d71c3
Ivan Hernandez <ivanhm84@gmail.com>
2018-06-01 19:25:41 -0500

    Servicio hecho, header y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 //rutas
+import { app_routing } from './app.routes';
 
-import { app_routing } from './app.routes'
+//servicios
+import { InformacionService } from './services/informacion.service';
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -24,9 +27,12 @@ import { ItemComponent } from './components/item/item.component';
   ],
   imports: [
     BrowserModule,
-    app_routing
+    app_routing,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    InformacionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InformacionService } from '../../services/informacion.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,11 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
-anio:number = new Date().getFullYear();
+  anio:number = new Date().getFullYear();
+
+  constructor( public _is:InformacionService ){
+
+  }
   
 
 }
